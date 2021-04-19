ed12c7034a6527a6eb8ffe57851406792dd10927
JoKing <joebryant614@gmail.com>
2021-04-18 20:56:23 -0700

    js done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,14 @@
-// test variables
-var city = 'mesa'
-
 
 // gloabal variables
 var apiKey = '29d178bf984c3810ca4482bb3d2b6ab0';
 
 
 
-function displayDash(cityLat, cityLon) {
+function displayDash(cityLat, cityLon, city) {
 
+    // clear content area
+    $('#current-day').empty();
+    $('#forcast-five').empty();
     var displayApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&units=imperial&exclude=minutely,hourly,alerts&appid=' + apiKey;
 
     fetch(displayApiUrl)
@@ -16,6 +16,7 @@ function displayDash(cityLat, cityLon) {
             if (response.ok) {
                 response.json()
                     .then(function (data) {
+                        console.log(data);
                         // build current weather
 
                         // set varibles response
@@ -28,7 +29,8 @@ function displayDash(cityLat, cityLon) {
 
                         // create html elements
                         var title = $('<h2>').text(city + ' (' + currentDate.getMonth() + '/' + currentDate.getDate() + '/' + currentDate.getFullYear() + ')');
-                        var img = $('<img>').attr('scr', 'http://openweathermap.org/img/wn/' + currentIcon + '.png')
+                        var img = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + currentIcon + '.png')
+                        img.attr('alt', 'weather icon');
                         var temp = $('<p>').text('Temp: ' + currentTemp + ' °F');
                         var wind = $('<p>').text('Wind: ' + currentWs + ' MPH');
                         var humidity = $('<p>').text('Humidity: ' + currentHumidity + '%');
@@ -52,13 +54,14 @@ function displayDash(cityLat, cityLon) {
                             var forcastTemp = data.daily[i].temp.day;
                             var forcastWs = data.daily[i].wind_speed;
                             var forcastHumidity = data.daily[i].humidity;
-                            
+
                             // creat div for forcast
-                            var forcastDiv = $('<div>').addClass('col-3 m-1 forcast');
+                            var forcastDiv = $('<div>').addClass('col-md-3 m-1 forcast');
 
                             // create html elements
-                            var title1 = $('<h3>').text('(' + forcastDate.getMonth() + '/' + forcastDate.getDate() + '/' + forcastDate.getFullYear() + ')');
-                            var img1 = $('<img>').attr('scr', 'http://openweathermap.org/img/wn/' + forcastIcon + '.png')
+                            var title1 = $('<h4>').text('(' + forcastDate.getMonth() + '/' + forcastDate.getDate() + '/' + forcastDate.getFullYear() + ')');
+                            var img1 = $('<img>').attr('src', 'http://openweathermap.org/img/wn/' + forcastIcon + '.png')
+                            img1.attr('alt', 'weather icon');
                             var temp1 = $('<p>').text('Temp: ' + forcastTemp + ' °F');
                             var wind1 = $('<p>').text('Wind: ' + forcastWs + ' MPH');
                             var humidity1 = $('<p>').text('Humidity: ' + forcastHumidity + '%');
@@ -87,23 +90,97 @@ function displayDash(cityLat, cityLon) {
 
 }
 
-// this will eventually be my submit function 
-var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey
 
-fetch(apiUrl)
-    .then(function (response) {
-        if (response.ok) {
-            response.json()
-                .then(function (data) {
-                    var cityLon = data.coord.lon;
-                    var cityLat = data.coord.lat;
+function getCoord(city) {
 
-                    displayDash(cityLat, cityLon)
-                });
-        } else {
-            alert("Error:" + response.statusText);
+    // this will eventually be my submit function 
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        var cityLon = data.coord.lon;
+                        var cityLat = data.coord.lat;
+
+                        displayDash(cityLat, cityLon, city)
+                    });
+            } else {
+                alert("Error:" + response.statusText);
+            }
+        });
+
+};
+
+function saveCity(city) {
+    // create button with class
+    var savedCity = $("<button>").text(city).addClass('saved-button')
+
+    // apend to page
+    $('#search-history').append(savedCity);
+
+    // check if their is a local storage
+    var cityList = localStorage.getItem("cityList");
+
+    if (cityList === null) {
+
+        var listObj = JSON.stringify([{ city: city }]);
+        var cityList = localStorage.setItem("cityList", listObj);
+
+    } else {
+
+        cityList = JSON.parse(cityList);
+        cityList.push({ city: city });
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+    }
+
+};
+
+// event listener to start the page up
+$('#search-form').on('submit', function (event) {
+    // prevent refresh
+    event.preventDefault();
+
+    // create city var
+    var city = $('#city-input').val();
+    console.log(city);
+
+    // check to make sure somehting is inputed into the text area
+    if (city === ''){
+        alert('Please input a city.');
+        window.location.reload();
+    } else {
+    // call functions with that city 
+    getCoord(city);
+    saveCity(city);
+
+    // clear the text area of the input
+    $('#city-input').val('');
+    }
+});
+
+// loads list from local storage
+function loadCity() {
+
+    var cityList = localStorage.getItem("cityList");
+    if (cityList === null) {
+        console.log("empty");
+    } else {
+        var cityList = JSON.parse(cityList);
+
+        for (i = 0; i < cityList.length; i++) {
+            var cityButton = cityList[i].city;
+
+            // create button with class
+            var savedCity = $("<button>").text(cityButton).addClass('saved-button')
+
+            // apend to page
+            $('#search-history').append(savedCity);
+            ;
         }
-    });
+    }
+};
 
 
-
+loadCity();
